6544e39ef8b448fb2d3afd4d76b3a27ebf804837
Aavash Chhetri <aavashchhetri01@gmail.com>
2022-06-30 23:06:24 +0545

    Initialized Block

--- src/block.hpp
+++ src/block.hpp
@@ -5,4 +5,35 @@
 #ifndef MATADAAN_BLOCK_HPP
 #define MATADAAN_BLOCK_HPP
 
+
+#include <vector>
+#include <string>
+
+class Block {
+public:
+    Block(){};
+    Block(int index, std::string prevHash, std::string Hash, std::string timeStamp, std::vector<std::string> data);
+
+    std::string getPrevHash();
+    std::string getHash();
+    int getIndex();
+    std::vector<std::string> getData();
+    void toString();
+    std::string calculateHash();
+    std::string calculateHash(std::string inputStr);
+    std::string getStringToHash();
+    bool validProof(int last_proof, int proof);
+    int proofOfWork(int last_proof);
+    //json toJson(void); // needs to include json.hpp which will be created later for json parsering
+
+private:
+    int index;
+    std::string prevHash;
+    std::string blockHash;
+    std::string timeStamp;
+    std::vector<std::string> data;
+    int proof;
+};
+
+
 #endif //MATADAAN_BLOCK_HPP
