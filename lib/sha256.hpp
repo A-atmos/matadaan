c3312e85b4f2510d2750821270199ec04b673d6a
Aavash Chhetri <aavashchhetri01@gmail.com>
2022-06-30 23:06:08 +0545

    Init sha256 and added json functionality

--- lib/sha256.hpp
+++ lib/sha256.hpp
@@ -5,4 +5,19 @@
 #ifndef MATADAAN_SHA256_HPP
 #define MATADAAN_SHA256_HPP
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#include <openssl/sha.h>
+#include <openssl/ripemd.h>
+using namespace std;
+
+class sha256{
+public:
+    sha256(string);
+
+};
+
 #endif //MATADAAN_SHA256_HPP
