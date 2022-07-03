a936e3ab455c7161b17b423facf1676705a8e4ab
Aavash Chhetri <aavashchhetri01@gmail.com>
2022-07-03 21:52:41 +0545

    Basic functionality of blockchain

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "main.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Testing Blockchain..." << std::endl;
+    Blockchain blockchain(0);
+    std::cout << blockchain.getBlock(0).toJson()<<std::endl;
+
+    // Data must be a vector of string
+    std::vector<std::string> data;
+    data.push_back(sha256("2002"));
+    data.emplace_back("1");
+    auto hash_nonce_pair = blockUtils::findHash(1,blockchain.getLatestBlockHash(), data);
+    std::cout<<hash_nonce_pair.first<<","<<hash_nonce_pair.second<<std::endl;
+    blockchain.addBlock(blockchain.numOfBlocks(),blockchain.getLatestBlockHash(),hash_nonce_pair.first,hash_nonce_pair.second,data);
+    std::cout << blockchain.jsonDump()<<std::endl;
+    std::cout << voteUtils::isValidVoter(sha256("2002"),blockchain.toJson());
+
     return 0;
 }
