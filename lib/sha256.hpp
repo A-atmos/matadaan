09518ce02cea48bcfb50dd6b7889f7fe0ee6a929
Aavash Chhetri <aavashchhetri01@gmail.com>
2022-07-04 23:37:41 +0545

    Added the namespace

--- lib/sha256.hpp
+++ lib/sha256.hpp
@@ -13,39 +13,40 @@
 #include <openssl/sha.h>
 #include <openssl/ripemd.h>
 
-using namespace std;
+namespace hash{
+    std::string sha256(std::string str){
+        unsigned char hash[SHA256_DIGEST_LENGTH];
+        // use a SHA256_CTX structure which is includes unsigned int long values for SHA hashes
+        // It is required to initialize a SHA256 Hash using openssl
+        SHA256_CTX sha256;
 
-std::string sha256(std::string str){
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    // use a SHA256_CTX structure which is includes unsigned int long values for SHA hashes
-    // It is required to initialize a SHA256 Hash using openssl
-    SHA256_CTX sha256;
+        /*
+         * Used the above used structure to initialize a SHA256 hash
+         * */
+        SHA256_Init(&sha256);
+        /*
+         *can be called repeatedly with chunks of the message to be hashed (len bytes at data).
+         */
+        SHA256_Update(&sha256, str.c_str(), str.size());
+        /*
+         * places the message digest in md, which must have space for
+         * SHA_DIGEST_LENGTH == 20 bytes of output, and erases the SHA_CTX*/
 
-    /*
-     * Used the above used structure to initialize a SHA256 hash
-     * */
-    SHA256_Init(&sha256);
-    /*
-     *can be called repeatedly with chunks of the message to be hashed (len bytes at data).
-     */
-    SHA256_Update(&sha256, str.c_str(), str.size());
-    /*
-     * places the message digest in md, which must have space for
-     * SHA_DIGEST_LENGTH == 20 bytes of output, and erases the SHA_CTX*/
+        SHA256_Final(hash, &sha256);
 
-    SHA256_Final(hash, &sha256);
+        std::stringstream ss;
+        for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        {
+            ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+        }
 
-    stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+        return ss.str();
+
     }
-
-    return ss.str();
-
-};
+}
 //hex in iostream
 //SHA256_DIGEST_LENGTH, SHA256_CTX in openssl/sha.h
 //setw() and setfill() in iomanip
 //stringstream in sstream
+
 #endif //MATADAAN_SHA256_HPP
