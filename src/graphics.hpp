741f42016c5ec4634feed842c2cafa1471bb6b8f
Anmol chalise <anmolchalise@Anmols-MacBook-Air.local>
2022-07-21 16:04:14 +0545

    graphics 50%

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -1,20 +1,37 @@
 #include <gtkmm.h>
+#include <iostream>
+#include <cstring>
+//#include <vector>
+//#include<fstream>
+
+
+namespace superuser
+{
+    std::string id="admin";
+    std::string pwd="admin";
+
+}
+
 class Enter :public Gtk:: Window {
 public:
+
     void on_button_clicked();
+    int checkclicked();
+    bool checksuperuser();
+    void for_error();
 
     Enter()
     {
-        add(scrolledWindow);
+        add(scrolledWindow);                //adds window
         scrolledWindow.add(fixed);
 
-
+        //creates textbox for id
         textbox1.set_text("Enter Citizenship number");
         fixed.add(textbox1);
         fixed.move(textbox1, 240, 150);
-        //textbox1.set_size_request(50,10);
+        textbox1.set_size_request(50,10);
 
-
+        //creates textbox foe password
         textbox2.set_text("Password");
         fixed.add(textbox2);
         fixed.move(textbox2, 240, 200);
@@ -22,23 +39,23 @@ public:
 
 
 
-
+        //creates login button
         button.set_label("Log In");
         fixed.add(button);
         fixed.move(button, 280, 250);
         button.set_size_request(10,10);
 
         button.signal_clicked().connect(sigc::mem_fun(*this,
-                                                      &Enter::on_button_clicked));
+                                                      &Enter::on_button_clicked));//calls on_button_clicked function if login button is clicked
 
-        set_title("Matadaan Garau,Desh Banau");
+        set_title("Matadaan Garau,Desh Banau");       //sets window title
         move(320, 200);
         resize(640, 480);
 
-        show_all();
+        show_all();                                         //shows all widgets
     }
 
-
+    //asks if u wanna exit
     bool on_delete_event(GdkEventAny* any_event) override {
         Gtk::MessageDialog dialog(*this, "matadaan grnu vayo tw??", true, Gtk::MESSAGE_QUESTION, Gtk::BUTTONS_YES_NO, true);
 
@@ -50,24 +67,62 @@ public:
             return Window::on_delete_event(any_event);
         return true;
     }
-
 protected:
+    int count=0;
     Gtk::ScrolledWindow scrolledWindow;
 
     Gtk::Fixed fixed;
     Gtk::Entry textbox1;
     Gtk::Entry textbox2;
     Gtk::Button button;
-    Gtk::Button button1;
-    Gtk::Button button2;
+    Gtk::Button button0;
+
 };
-void  Enter::on_button_clicked()
+
+
+//is called when login button is clicked
+void  Enter::on_button_clicked(){
+    if(checksuperuser()==1) {
+        hide();
+        count++;
+    }
+    else
+    {
+        for_error();
+
+    }
+}
+
+
+//checks if login button is clicked
+int Enter::checkclicked()
 {
-    hide();
+    return count;
+}
 
 
+//checks if user is superuser
+bool Enter::checksuperuser()
+{
+    std::string id=textbox1.get_text();;
+    std::string pwd=textbox2.get_text();;
+    if(superuser::id==id && superuser::pwd==pwd)
+    {
+        return true;
+    }
+}
 
-};
+//prints error if login failed
+void Enter::for_error() {
+
+    button0.set_label("Error occured while logging in!");
+    fixed.add(button0);
+    fixed.move(button0, 80, 50);
+    button0.set_size_request(200,10);
+    show_all();
+}
+
+//used for creating window after loging in
 class login:public Gtk::Window
 {
 public:
@@ -79,7 +134,6 @@ public:
         fixed.add(button1);
         fixed.move(button1, 240, 150);
         button1.set_size_request(70, 10);
-
 
         button2.set_label("KP Oli");
         fixed.add(button2);
@@ -98,6 +152,7 @@ private:
     Gtk::Button button2;
     Gtk::Fixed fixed;
 
+    //asks if u wanna exit
     bool on_delete_event(GdkEventAny* any_event) override {
         Gtk::MessageDialog dialog(*this, "matadaan grnu vayo tw??", true, Gtk::MESSAGE_QUESTION, Gtk::BUTTONS_YES_NO, true);
 
@@ -112,22 +167,12 @@ private:
 
 
 };
+//class user
+//{
+//public:
+//    std::vector<std::string>id;
+//    std::vector<std::string>password;
+//    std::ofstream fout;
+//    std::fout.open("data.txt",app);
+//};
 
-int main (int argc, char *argv[])
-{
-    Gtk::Main kit(argc, argv);
-
-    Enter enter;
-
-    //Shows the window and returns when it is closed.
-    Gtk::Main::run(enter);
-    login log;
-
-    //Shows the window and returns when it is closed.
-    Gtk::Main::run(log);
-
-
-
-
-    return 0;
-}
