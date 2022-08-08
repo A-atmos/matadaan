6032a018a4cec4eb7777b2c4126976388c7c530f
A-atmos <aavashchhetri01@gmail.com>
2022-08-08 08:22:51 +0545

    Managed the header files for main.cpp

--- main.h
+++ main.h
@@ -4,6 +4,18 @@
 
 #ifndef MATADAAN_MAIN_H
 #define MATADAAN_MAIN_H
-#include "src/blockchain.hpp"
+
+#include "lib/json.hpp"
+
+
 #include "src/simple_http/http_server.hpp"
+
+#include "src/blockchain.hpp"
+
+#include "src/graphics.hpp"
+#include "src/userModel.hpp"
+
+#include <thread>
+
+
 #endif //MATADAAN_MAIN_H
