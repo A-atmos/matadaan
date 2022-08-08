02aa0e9cc4d5f6f3694e4538fb5d31f6ff9bed0a
A-atmos <aavashchhetri01@gmail.com>
2022-08-08 08:21:43 +0545

    Reformatted the code

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -1,54 +1,66 @@
 #include <gtkmm.h>
 #include <iostream>
 #include <cstring>
+#include <utility>
 #include <vector>
 #include<fstream>
 
+
+#include "utils.hpp"
+#include "../lib/json.hpp"
+#include "userModel.hpp"
+#include "blockchain.hpp"
 
 namespace superuser
 {
     std::string id="admin";
     std::string pwd="admin";
 
-
 }
+
+
 
 class Enter :public Gtk:: Window{
 public:
+    bool loggedIn;
+    bool running;
     void show_login_page();
     void on_button_clicked();
-    int checkclicked();
+    bool checkclicked();
     bool checksuperuser();
-    void for_error();
-     bool check_user();
+    void loginError(std::string errorMessage);
+    USER::User loggedUser();
 
+    Enter(Blockchain::Blockchain& _blockchain){
+        // loads the user data from file and make it the vector of users
+        user=USER::loadData("/home/ac/CLionProjects/matadaan/data.test");
+        blockchain = _blockchain.toJson();
 
-    Enter(){
         add(scrolledWindow);                //adds window
         scrolledWindow.add(fixed);
 
         //creates textbox for id
-        textbox1.set_text("Enter Citizenship number");
-        fixed.add(textbox1);
-        fixed.move(textbox1, 240, 150);
-        textbox1.set_size_request(50,10);
+        usernameTextbox.set_text("Enter Citizenship number");
+        fixed.add(usernameTextbox);
+        fixed.move(usernameTextbox, 240, 150);
+        usernameTextbox.set_size_request(50,10);
 
-        //creates textbox foe password
-        textbox2.set_text("Password");
-        fixed.add(textbox2);
-        fixed.move(textbox2, 240, 200);
-        textbox2.set_size_request(170,10);
+        //creates textbox for password
+        passwordTextbox.set_text("Password");
+        fixed.add(passwordTextbox);
+        fixed.move(passwordTextbox, 240, 200);
+        passwordTextbox.set_size_request(170,10);
 
 
 
         //creates login button
-        button.set_label("Log In");
-        fixed.add(button);
-        fixed.move(button, 280, 250);
-        button.set_size_request(10,10);
+        loginButton.set_label("Log In");
+        fixed.add(loginButton);
+        fixed.move(loginButton, 280, 250);
+        loginButton.set_size_request(10,10);
 
-        button.signal_clicked().connect(sigc::mem_fun(*this,
-                                                      &Enter::on_button_clicked));//calls on_button_clicked function if login button is clicked
+        loginButton.signal_clicked().connect(sigc::mem_fun(*this,
+                                                           &Enter::on_button_clicked));//calls on_button_clicked function if login button is clicked
 
         set_title("Matadaan Garau,Desh Banau");       //sets window title
         move(320, 200);
@@ -67,48 +79,69 @@ public:
         dialog.set_position(Gtk::WindowPosition::WIN_POS_CENTER);
         if (dialog.run() == Gtk::RESPONSE_YES)
             return Window::on_delete_event(any_event);
+        running = false;
         return true;
     }
+
 private:
-    int count=0;
+
     Gtk::ScrolledWindow scrolledWindow;
-
     Gtk::Fixed fixed;
-    Gtk::Entry textbox1;
-    Gtk::Entry textbox2;
-    Gtk::Button button;
-    Gtk::Button button0;
-
+    Gtk::Entry usernameTextbox;
+    Gtk::Entry passwordTextbox;
+    Gtk::Button loginButton;
+    Gtk::Button errorButton;
+    std::vector<USER::User> user;
+    nlohmann::json blockchain;
 };
 
 
 
 //is called when login button is clicked
 void  Enter::on_button_clicked(){
-    if(checksuperuser()==1 || check_user()==1) {
+    try{
+        if(!loggedUser().exists()){
+            throw USER::UserDoesnotExist();
+        }
+
+        if(loggedUser().alreadyVoted(blockchain)){
+            throw USER::UserAlreadyVoted();
+        }
         hide();
-        count++;
+        loggedIn = true;
+    }
+    catch(USER::UserAlreadyVoted){
+        loginError("You have already voted!");
+    }
+    catch(USER::UserDoesnotExist){
+        loginError("Please Register to Vote!");
     }
 
-    else
-    {
-        for_error();
-
-    }
 }
 
+USER::User Enter::loggedUser() {
+
+    for(int i = 0; i<user.size(); i++){
+        // std::cout<<user[i].id()<<":"<<user[i].password()<<std::endl;
+        if(user[i].id() == usernameTextbox.get_text() && user[i].password() == passwordTextbox.get_text()){
+            return user[i];
+        }
+    }
+    loggedIn = false;
+    return {};
+}
 
 //checks if login button is clicked
-int Enter::checkclicked()
+bool Enter::checkclicked()
 {
-    return count;
+    return loggedIn;
 }
 
 //checks if user is superuser
 bool Enter::checksuperuser()
 {
-    std::string id=textbox1.get_text();;
-    std::string pwd=textbox2.get_text();;
+    std::string id=usernameTextbox.get_text();;
+    std::string pwd=passwordTextbox.get_text();;
     if(superuser::id==id && superuser::pwd==pwd)
     {
         return true;
@@ -116,166 +149,94 @@ bool Enter::checksuperuser()
 }
 
 //prints error if login failed
-void Enter::for_error() {
+void Enter::loginError(std::string errorMessage) {
 
-    button0.set_label("Error occured while logging in!");
-    fixed.add(button0);
-    fixed.move(button0, 80, 50);
-    button0.set_size_request(200,10);
+    errorButton.set_label(errorMessage);
+    fixed.add(errorButton);
+    fixed.move(errorButton, 80, 50);
+    errorButton.set_size_request(200,10);
     show_all();
 }
 
-//used for creating window after loging in
-class login:public Gtk::Window
-{
+
+//used for creating window after logging in
+
+class voteWindow: public Gtk::Window{
 public:
-    login() {
-        add(scrolledWindow1);
-        scrolledWindow1.add(fixed);
+    voteWindow(USER::User _user, Blockchain::Blockchain& _blockchain);
+    virtual ~voteWindow();
 
-        button1.set_label("Sher bahadur Deuwa          ");
-        fixed.add(button1);
-        fixed.move(button1, 240, 150);
-        button1.set_size_request(70, 10);
-
-        button2.set_label("KP Oli");
-        fixed.add(button2);
-        fixed.move(button2, 240, 200);
-        button2.set_size_request(170, 10);
-
-        set_title("momo channa 10 min vnye,neta channa kina hatar");
-        move(320, 200);
-        resize(640, 480);
-
-        show_all();
-    }
+protected:
+    void on_button_click(int,int, Blockchain::Blockchain& _blockchain);
 private:
-    Gtk::ScrolledWindow scrolledWindow1;
-    Gtk::Button button1;
-    Gtk::Button button2;
-    Gtk::Fixed fixed;
-
-    //asks if u wanna exit
-    bool on_delete_event(GdkEventAny* any_event) override {
-        Gtk::MessageDialog dialog(*this, "matadaan grnu vayo tw??", true, Gtk::MESSAGE_QUESTION, Gtk::BUTTONS_YES_NO, true);
-
-        dialog.set_title("matadaan grnu vayo tw??");
-        dialog.set_modal();
-        dialog.set_size_request(100,20);
-        dialog.set_position(Gtk::WindowPosition::WIN_POS_CENTER);
-        if (dialog.run() == Gtk::RESPONSE_YES)
-            return Window::on_delete_event(any_event);
-        return true;
-    }
-};
-
-
-
-class User{
-public:
-    std::string id;
-    std::string password;
-    std::string line;
-    std::vector<std::string>data;
-
-    User(){}
-    User(std::string id,std::string password)
-    {
-        this->id=id;
-        this->password=password;
-    }
-    void input_data(std::string filename);
-    void  get_file_content(std::string file);
-
-    ~User(){}
-
+    std::vector<Gtk::Button*> board;
+    USER::User currentUser;
+    std::vector<CANDIDATE::candidate> candidates;
 
 };
-void input_data(std::string filename)
-{
-    std::string line;
-    std::ofstream fout(filename.c_str(), std::ios::app);
-    while(fout)
-    {
-        getline(std::cin,line);
-        if(line=="q")
-            break;
-        fout<<line<<",";
-    }
-    fout.close();
-}
-void  User::get_file_content(std::string file)
-{
+
+voteWindow::voteWindow(USER::User _user,Blockchain::Blockchain& _blockchain) {
+
+    currentUser = _user;
+    candidates = CANDIDATE::loadFromFile();
+    set_default_size(600,600);
+    set_title("Please Vote to your favourite candidate!");
+
+    Gtk::Box *vbox = Gtk::manage(new Gtk::VBox);
+    add(*vbox);
+
+    int height = candidates.size()/8;
+    height++;
+
+    int h_pos=0,v_pos=0;
 
 
-    std::ifstream fin(file.c_str(),std::ios::in);
-    if(!fin)
-    {
-        std::cerr<<"Error occured";
-    }
-    while(fin)
-    {
+    for(int i =0; i<height;++i){
 
-        getline(fin,line,',');
-        data.push_back(line);
+        Gtk::HBox* hbox = Gtk::manage(new Gtk::HBox);
+        hbox->set_hexpand(true);
 
-    }
+        while(!(h_pos==8 || i*8+h_pos == candidates.size())){
+            Gtk::Image *image;
+            image = Gtk::manage(new Gtk::Image{candidates[i*8+h_pos].getImagePath()});
+            // image->property_width_request(100);
+            // image->property_height_request(100);
+
+            auto position = Gtk::manage(new Gtk::Button);
+            position->set_image(*image);
+            position->set_label(candidates[i*8+h_pos].getName());
 
 
+            board.push_back(position);
 
-}
-bool Enter::check_user() {
+            position->signal_clicked().connect(
+                    [this,h_pos,i,&_blockchain] {this->on_button_click(h_pos,i,_blockchain);});
 
-    User u1;
-    std::vector<User> user;
-    u1.get_file_content("data.txt");
-
-    std::string s1;
-    std::string s2;
-    int j = 1;
-    for (std::string &i: u1.data) {
-
-        if (j % 2 != 0) {
-            s1 = i;
-        } else {
-            s2 = i;
-            User u(s1, s2);
-            user.push_back(u);
-            s1.clear();
-            s2.clear();
-
+            hbox->add(*position);
+            h_pos++;
         }
-        j++;
-
+        h_pos = 0;
+        vbox->add(*hbox);
     }
 
 
-    std::vector<User>::iterator it;
-    for (it = user.begin(); it != user.end(); ++it) {
-        if (it->id == textbox1.get_text() && it->password == textbox2.get_text()) {
-            return 1;
-        }
-    }
+
+    vbox->show_all();
 }
-int main (int argc, char *argv[]) {
-    Gtk::Main kit(argc, argv);
-
-    Enter enter;
 
 
-    //Shows the window and returns when it is closed.
-    Gtk::Main::run(enter);
+void voteWindow::on_button_click(int x,int y, Blockchain::Blockchain& _blockchain) {
+    std::cout<<"("<<x<<","<<y<<")"<<std::endl;
 
+    std::vector<std::string> data;
+    std::cout<<currentUser.id()<<":"<<hash::sha256(currentUser.id())<<":"<<candidates[x+y*8].getName()<<std::endl;
+    data.push_back(hash::sha256(currentUser.id()));
+    data.emplace_back(candidates[x+y*8].getName());
 
-    if (enter.checkclicked() == 1) {
-        //Shows the window and returns when it is closed.
+    auto hash_nonce_pair = _blockchain.findNewHash(data);
 
-        login log;
-        Gtk::Main::run(log);
-
-
-    }
-
-
-    return 0;
+    _blockchain.addBlock(_blockchain.numOfBlocks(),_blockchain.getLatestBlockHash(),hash_nonce_pair.first,hash_nonce_pair.second,data);
+    hide();
 }
+
+voteWindow::~voteWindow(){}
