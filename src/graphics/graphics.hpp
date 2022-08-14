43dd7658a8e5fa60f9be376de936c2959d6c7f8b
A-atmos <aavashchhetri01@gmail.com>
2022-08-15 04:28:17 +0545

    Error handling and minor changes while registering users and candidates

--- src/graphics/graphics.hpp
+++ src/graphics/graphics.hpp
@@ -33,48 +33,62 @@ public:
 
     Enter(Blockchain::Blockchain& _blockchain){
         // loads the user data from file and make it the vector of users
-        user=USER::loadData("user.txt");
-        blockchain = _blockchain.toJson();
+        try {
+            user = USER::loadData("user.txt");
+            blockchain = _blockchain.toJson();
 
-        add(scrolledWindow);                //adds window
-        scrolledWindow.add(fixed);
+            add(scrolledWindow);                //adds window
+            scrolledWindow.add(fixed);
 
-        //creates textbox for id
-        label1.set_text("Citizenship No:");
-        fixed.add(label1);
-        fixed.move(label1, 240, 100);
-        label1.set_size_request(10, 10);
-        usernameTextbox.set_text("");
-        fixed.add(usernameTextbox);
-        fixed.move(usernameTextbox, 240, 130);
-        usernameTextbox.set_size_request(50,10);
+            //creates textbox for id
+            label1.set_text("Citizenship No:");
+            fixed.add(label1);
+            fixed.move(label1, 240, 100);
+            label1.set_size_request(10, 10);
+            usernameTextbox.set_text("");
+            fixed.add(usernameTextbox);
+            fixed.move(usernameTextbox, 240, 130);
+            usernameTextbox.set_size_request(50, 10);
 
-        //creates textbox for password
-        label2.set_text("Password:");
-        fixed.add(label2);
-        fixed.move(label2, 240, 180);
-        label2.set_size_request(10, 10);
-        passwordTextbox.set_text("");
-        fixed.add(passwordTextbox);
-        fixed.move(passwordTextbox, 240, 200);
-        passwordTextbox.set_size_request(170,10);
+            //creates textbox for password
+            label2.set_text("Password:");
+            fixed.add(label2);
+            fixed.move(label2, 240, 180);
+            label2.set_size_request(10, 10);
+            passwordTextbox.set_text("");
+            fixed.add(passwordTextbox);
+            fixed.move(passwordTextbox, 240, 200);
+            passwordTextbox.set_size_request(170, 10);
 
 
 
-        //creates login button
-        loginButton.set_label("Log In");
-        fixed.add(loginButton);
-        fixed.move(loginButton, 280, 250);
-        loginButton.set_size_request(10,10);
+            //creates login button
+            loginButton.set_label("Log In");
+            fixed.add(loginButton);
+            fixed.move(loginButton, 280, 250);
+            loginButton.set_size_request(10, 10);
 
-        loginButton.signal_clicked().connect(sigc::mem_fun(*this,
-                                                           &Enter::on_button_clicked));//calls on_button_clicked function if login button is clicked
+            loginButton.signal_clicked().connect(sigc::mem_fun(*this,
+                                                               &Enter::on_button_clicked));//calls on_button_clicked function if login button is clicked
 
-        set_title("Matadaan Garau,Desh Banau");       //sets window title
-        move(320, 200);
-        resize(640, 480);
+            set_title("Matadaan Garau,Desh Banau");       //sets window title
+            move(320, 200);
+            resize(640, 480);
 
-        show_all();
+            show_all();
+        }
+        catch(...){
+            Gtk::MessageDialog dialog(*this, "Error: Please Check if there is a users list!", true, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_CLOSE, true);
+
+            dialog.set_title("Error");
+            dialog.set_modal();
+            dialog.set_size_request(100,20);
+            dialog.set_position(Gtk::WindowPosition::WIN_POS_CENTER);
+            if(dialog.run() == Gtk::RESPONSE_CLOSE){
+                hide();
+                exit(1);
+            }
+        }
     }
 
     //asks if u wanna exit
@@ -193,73 +207,84 @@ private:
 
 voteWindow::voteWindow(USER::User _user,Blockchain::Blockchain& _blockchain) {
 
-    currentUser = _user;
-    candidates = CANDIDATE::loadFromFile();
-    set_default_size(600,600);
-    set_title("Please Vote to your favourite candidate!");
+    try {
+        currentUser = _user;
+        candidates = CANDIDATE::loadFromFile();
+        this->fullscreen();
+        set_title("Please Vote to your favourite candidate!");
 
-    Gtk::Box *vbox = Gtk::manage(new Gtk::VBox);
-    add(*vbox);
-
-
-    auto msg = Gtk::manage(new Gtk::Label("Federal Elections of Nepal - 2080"));
-    auto attrlist = pango_attr_list_new();
-
-    PangoFontDescription * font_desc = pango_font_description_new();
-    pango_font_description_set_size(font_desc, 36 * PANGO_SCALE);
-    PangoAttribute * attr = pango_attr_font_desc_new(font_desc);
-    pango_attr_list_insert(attrlist, attr);
-
-    msg->set_hexpand(true);
-    auto list = Pango::AttrList(attrlist);
-    msg->set_attributes(list);
-
-    vbox->add(*msg);
+        Gtk::Box *vbox = Gtk::manage(new Gtk::VBox);
+        add(*vbox);
 
 
-    int height = candidates.size()/8;
-    height++;
+        auto msg = Gtk::manage(new Gtk::Label("Federal Elections of Nepal - 2080"));
+        auto attrlist = pango_attr_list_new();
 
-    int h_pos=0;
+        PangoFontDescription *font_desc = pango_font_description_new();
+        pango_font_description_set_size(font_desc, 36 * PANGO_SCALE);
+        PangoAttribute *attr = pango_attr_font_desc_new(font_desc);
+        pango_attr_list_insert(attrlist, attr);
 
+        msg->set_hexpand(true);
+        auto list = Pango::AttrList(attrlist);
+        msg->set_attributes(list);
 
-    for(int i =0; i<height;++i){
-
-        Gtk::HBox* hbox = Gtk::manage(new Gtk::HBox);
-        hbox->set_hexpand(true);
-
-        while(!(h_pos==8 || i*8+h_pos == candidates.size())){
-            Gtk::Image *image;
-            image = Gtk::manage(new Gtk::Image{candidates[i*8+h_pos].getImagePath()});
-            // image->property_width_request(100);
-            // image->property_height_request(100);
-
-            auto position = Gtk::manage(new Gtk::Button);
-            position->set_image(*image);
-            position->set_label(candidates[i*8+h_pos].getName());
+        vbox->add(*msg);
 
 
-            board.push_back(position);
+        int height = candidates.size() / 8;
+        height++;
 
-            position->signal_clicked().connect(
-                    [this,h_pos,i,&_blockchain] {this->on_button_click(h_pos,i,_blockchain);});
+        int h_pos = 0;
 
-            hbox->add(*position);
-            h_pos++;
+
+        for (int i = 0; i < height; ++i) {
+
+            Gtk::HBox *hbox = Gtk::manage(new Gtk::HBox);
+            hbox->set_hexpand(true);
+
+            while (!(h_pos == 8 || i * 8 + h_pos == candidates.size())) {
+                Gtk::Image *image;
+                image = Gtk::manage(new Gtk::Image{candidates[i * 8 + h_pos].getImagePath()});
+                // image->property_width_request(100);
+                // image->property_height_request(100);
+
+                auto position = Gtk::manage(new Gtk::Button);
+                position->set_image(*image);
+                position->set_label(candidates[i * 8 + h_pos].getName());
+
+
+                board.push_back(position);
+
+                position->signal_clicked().connect(
+                        [this, h_pos, i, &_blockchain] { this->on_button_click(h_pos, i, _blockchain); });
+
+                hbox->add(*position);
+                h_pos++;
+            }
+            h_pos = 0;
+            vbox->add(*hbox);
         }
-        h_pos = 0;
-        vbox->add(*hbox);
+
+
+        vbox->show_all();
+
+    }catch(...){
+        Gtk::MessageDialog dialog(*this, "Error: Please Check if there is a candidates list!", true, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_CLOSE, true);
+
+        dialog.set_title("Error");
+        dialog.set_modal();
+        dialog.set_size_request(100,20);
+        dialog.set_position(Gtk::WindowPosition::WIN_POS_CENTER);
+        if(dialog.run() == Gtk::RESPONSE_CLOSE){
+            hide();
+            exit(1);
+        }
     }
-
-
-
-    vbox->show_all();
 }
 
 
 void voteWindow::on_button_click(int x,int y, Blockchain::Blockchain& _blockchain) {
-    std::cout<<"("<<x<<","<<y<<")"<<std::endl;
-
     Gtk::MessageDialog dialog(*this, "Do you want to confirm your vote?", true, Gtk::MESSAGE_QUESTION, Gtk::BUTTONS_YES_NO, true);
 
     dialog.set_title("Vote Confirmation");
